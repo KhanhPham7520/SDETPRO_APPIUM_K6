75b47d2aafcce6874d52271d2a9a095088858e25
phamphannhatkhanh7520@gmail.com <phamphannhatkhanh7520@gmail.com>
2022-02-28 23:38:03 +0700

    Completed lab 3

---
+++ src/lesson_03_exercise/lab_3_2.java
@@ -0,0 +1,22 @@
+package lesson_03_exercise;
+
+public class lab_3_2 {
+
+    public static void main(String[] args) {
+        int[] intArr = {1,2,3,4,5};
+        int minNumber = intArr[0];
+        int maxNumber = intArr[0];
+
+        for(int i = 0; i < intArr.length; i++){
+            if(intArr[i] < minNumber){
+                minNumber = intArr[i];
+            }
+            if(intArr[i] > maxNumber){
+                maxNumber = intArr[i];
+            }
+        }
+        System.out.println("Min number : "+minNumber);
+        System.out.println("Max number : "+maxNumber);
+
+    }
+}
